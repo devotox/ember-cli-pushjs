5b4f2319edfdd9eedb64bbe98561e2e10e6ee105
Devonte <devo.tox.89@gmail.com>
2018-04-12 11:27:41 +0100

    Upgrade

--- index.js
+++ index.js
@@ -1,5 +1,71 @@
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+const fastbootTransform = require('fastboot-transform');
+
 module.exports = {
-  name: 'ember-cli-pushjs'
+	name: 'ember-cli-pushjs',
+	options: {
+		nodeAssets: {
+			'push-fcm-plugin': {
+				vendor: {
+					srcDir: 'bin',
+					include: ['push.fcm.js'],
+					processTree(input) {
+						return fastbootTransform(input);
+					}
+				}
+			}
+		}
+	},
+
+	included() {
+		this._super.included.apply(this, arguments);
+		this._ensureThisImport();
+
+		this.import('vendor/shims/push.js');
+		this.import('node_modules/push.js/bin/push.js');
+		this.import('node_modules/firebase/firebase.js');
+		this.import('vendor/push-fcm-plugin/push.fcm.js');
+	},
+
+	treeForPublic(tree) {
+		let push = new Funnel('node_modules/push.js/bin', {
+			include: ['serviceWorker.js'],
+			destDir: 'push.js'
+		});
+
+		let fcm = new Funnel('node_modules/push-fcm-plugin/bin', {
+			include: ['firebase-messaging-sw.min.js'],
+			destDir: 'push.js'
+		});
+
+		return mergeTrees([push, fcm]);
+	},
+
+	_getAddonOptions() {
+		return this.parent && this.parent.options
+			|| this.app && this.app.options || {};
+	},
+
+	_ensureThisImport() {
+		if (!this.import) {
+			this._findHost = function findHostShim() {
+				let current = this;
+				let app;
+
+				// eslint-disable-next-line
+				do {
+					app = current.app || app;
+				} while (current.parent.parent && (current = current.parent));
+
+				return app;
+			};
+			this.import = function importShim(asset, options) {
+				let app = this._findHost();
+				app.import(asset, options);
+			};
+		}
+	}
 };
